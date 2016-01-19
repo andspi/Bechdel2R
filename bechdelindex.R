e192dc397c1ca4f5ad3bcd46bcb4543e8cdb0ef6
andspi <andietat@gmail.com>
2016-01-19 10:24:22 +0100

    Skript for basic index creation and transformation done

---
+++ bechdelindex.R
@@ -0,0 +1,17 @@
+# Script to create index of bechdeltest.com for further API analyses
+
+Bechdeltest_index_to_DF <- function(){
+
+  # load packages
+  require(httr)
+  require(jsonlite)
+  
+  #request data
+  bech_index_resp <- GET("http://bechdeltest.com/api/v1/getAllMovieIds")
+  
+  # extract to data frame
+  bech_index_DF <- fromJSON(content(bech_index_resp, as="text"))
+  head(bech_index_DF)
+  
+  
+}
